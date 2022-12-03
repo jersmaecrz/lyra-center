dc46d8285085a2ebbcef528d5dd80e66a918ff28
lei <82316160+leilapatricia@users.noreply.github.com>
2022-12-03 22:28:11 +0800

    Admin Dashboard Front-end & DB Connection

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php 
+    $host = "remotemysql.com";
+    $user = "NsqxjSUDbx";
+    $password = "wLgUwnheJo";
+    $dbName = "NsqxjSUDbx" ;
+    //Connect to database using PDO 
+    try {
+        $dsn ="mysql:host=$host;dbname=$dbName";
+        $pdo = new PDO($dsn,$user, $password);
+        //Check the connection 
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        echo "Successfully Connected to the Database.";
+    } catch(PDOException $e) {
+        echo "Unable to Connect to DB: ". $e->getMessage();
+    }
+?>
